9bbf1360d292b03d05de879197ac5554d839a580
GuzRam <lalubagoes2@gmail.com>
2023-03-23 15:30:48 +0700

    Pisah LoginPage dari main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'pages/Login.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -30,77 +31,7 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ListView(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          padding: EdgeInsets.all(30),
-          children: [
-            TextField(
-              controller: email,
-              autocorrect: false,
-              decoration: InputDecoration(
-                  labelText: "Email",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: pass,
-              autocorrect: false,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-              keyboardType: TextInputType.text,
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (email.text == "Hello" && pass.text == "123") {
-                  print("Log in Succesfull...");
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-//                    title: Text("Alert"),
-                      content: Text("Login Succesfull"),
-                    ),
-                  );
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Home(),
-                  ));
-                } else {
-                  print("Failed to Log in...");
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-//                   title: Text("Dialog"),
-                      content: Text("Login Failed"),
-                    ),
-                  );
-                }
-              },
-              child: Text("Log In"),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Login(email: email, pass: pass);
   }
 }
+
---
+++ lib/pages/login.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+class Login extends StatelessWidget {
+  const Login({
+    super.key,
+    required this.email,
+    required this.pass,
+  });
+
+  final TextEditingController email;
+  final TextEditingController pass;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          padding: EdgeInsets.all(30),
+          children: [
+            TextField(
+              controller: email,
+              autocorrect: false,
+              decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: pass,
+              autocorrect: false,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+              keyboardType: TextInputType.text,
+              obscureText: true,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (email.text == "Hello" && pass.text == "123") {
+                  print("Log in Succesfull...");
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+//                    title: Text("Alert"),
+                      content: Text("Login Succesfull"),
+                    ),
+                  );
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ));
+                } else {
+                  print("Failed to Log in...");
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+//                   title: Text("Dialog"),
+                      content: Text("Login Failed"),
+                    ),
+                  );
+                }
+              },
+              child: Text("Log In"),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
